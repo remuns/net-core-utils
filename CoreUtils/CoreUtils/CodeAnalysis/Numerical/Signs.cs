fd00808bcab3f9d973ff055f25675ab9d2cc82ae
Richard Munson <rmunson@ovitz.us>
2021-09-18 13:42:16 -0400

    Added a series of sign numerical attributes to the project
    These attributes describe the sign of a value regardless of what the corresponding type allows.

---
+++ CoreUtils/CoreUtils/CodeAnalysis/Numerical/Signs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace REMuns.CoreUtils.CodeAnalysis.Numerical
+{
+    /// <summary>
+    /// Indicates a detail about the sign of a numerical return value, parameter or property,
+    /// even if the corresponding type allows the detail to be false.
+    /// </summary>
+    [AttributeUsage(
+        AttributeTargets.ReturnValue
+            | AttributeTargets.Parameter
+            | AttributeTargets.Property,
+        AllowMultiple = false,
+        Inherited = false)]
+    public abstract class SignAttribute : Attribute { }
+
+    /// <summary>
+    /// Indicates that a return value, parameter or property is positive even if the corresponding
+    /// type allows non-positive values.
+    /// </summary>
+    public class PositiveAttribute : SignAttribute { }
+
+    /// <summary>
+    /// Indicates that a return value, parameter or property is negative even if the corresponding
+    /// type allows non-negative values.
+    /// </summary>
+    public class NegativeAttribute : SignAttribute { }
+
+    /// <summary>
+    /// Indicates that a return value, parameter or property is non-negative even if the
+    /// corresponding type allows negative values.
+    /// </summary>
+    public class NonNegativeAttribute : SignAttribute { }
+
+    /// <summary>
+    /// Indicates that a return value, parameter or property is non-positive even if the
+    /// corresponding type allows positive values.
+    /// </summary>
+    public class NonPositiveAttribute : SignAttribute { }
+}
