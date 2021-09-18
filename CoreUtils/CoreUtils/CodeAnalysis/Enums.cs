5cee3b0640f7cde4912ba2990a010ec95aa9dc68
Richard Munson <rmunson@ovitz.us>
2021-09-18 14:08:16 -0400

    Added a defined enum value attribute

---
+++ CoreUtils/CoreUtils/CodeAnalysis/Enums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace REMuns.CoreUtils.CodeAnalysis
+{
+    /// <summary>
+    /// Indicates that the enumeration-type property, field or return value is always a named,
+    /// defined value of the enumeration type.
+    /// </summary>
+    [AttributeUsage(
+            AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.ReturnValue,
+            AllowMultiple = false,
+            Inherited = false)]
+    public class NamedEnumValueAttribute : Attribute { }
+}
