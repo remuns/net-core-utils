5320cc24e30d99684f54a66f16948b73c6973db4
Richard Munson <rmunson@ovitz.us>
2021-09-19 10:03:30 -0400

    Added `Enumerables` static extension class
    The class currently contains a `Rotate` method that will rotate a collection by a certain number of places.
    
    The behavior is similar to `Enumerable.Skip` when too many places are rotated; if the number of places exceeds the length of the collection, the original collection is returned.  This is to avoid counting the collection.

---
+++ CoreUtils/CoreUtils.Test/Extensions/EnumerablesTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using REMuns.CoreUtils.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REMuns.CoreUtils.Test.Extensions
+{
+    [TestClass]
+    public class EnumerablesTest
+    {
+        private static Random Random { get; } = new();
+
+        /// <summary>
+        /// Tests that the rotation method works on zero length collections without any exceptions.
+        /// </summary>
+        /// <remarks>
+        /// This is an edge case test.
+        /// </remarks>
+        [TestMethod, TestCategory(nameof(Enumerables.Rotate))]
+        public void TestRotate_Empty()
+        {
+            var arr = Array.Empty<int>();
+            Assert.IsTrue(arr.Rotate(0).SequenceEqual(arr));
+            Assert.IsTrue(arr.Rotate(4).SequenceEqual(arr));
+        }
+
+        /// <summary>
+        /// Tests the rotation method on general non-empty collections when rotating more elements
+        /// than the length of the collection, in order to ensure the original collection is
+        /// returned in this case.
+        /// </summary>
+        [TestMethod, TestCategory(nameof(Enumerables.Rotate))]
+        public void TestRotate_TooManyPlaces()
+        {
+            var arr = new int[] { Random.Next(), Random.Next(), Random.Next() };
+            Assert.IsTrue(arr.Rotate(10).SequenceEqual(arr));
+        }
+
+        /// <summary>
+        /// Tests the rotation method on general non-empty collections.
+        /// </summary>
+        [TestMethod, TestCategory(nameof(Enumerables.Rotate))]
+        public void TestRotate_NonEmpty()
+        {
+            var arr = new int[] { Random.Next(), Random.Next(), Random.Next(), Random.Next() };
+            Assert.IsTrue(arr.Rotate(2).SequenceEqual(arr.Skip(2).Concat(arr.Take(2))));
+        }
+    }
+}
---
+++ CoreUtils/CoreUtils/Extensions/Enumerables.cs
@@ -0,0 +1,66 @@
+﻿using REMuns.CoreUtils.Control.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace REMuns.CoreUtils.Extensions
+{
+    /// <summary>
+    /// Static and extension methods for working with <see cref="IEnumerable{T}"/> instances.
+    /// </summary>
+    public static class Enumerables
+    {
+        /// <summary>
+        /// Rotates the current <see cref="IEnumerable{T}"/> by a specified number of places.
+        /// </summary>
+        /// <remarks>
+        /// Note that rotation is not cyclical; that is, if the number of places chosen exceeds the
+        /// count of the collection, the original collection will be returned, rather than allowing
+        /// the rotation to start again at the beginning when the count has been reached.
+        /// </remarks>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="places">The number of places to rotate by.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="source"/> was null.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="places"/> was negative.
+        /// </exception>
+        public static IEnumerable<TSource> Rotate<TSource>(
+            this IEnumerable<TSource> source, int places)
+        {
+            Throw.IfArgumentNull(source, nameof(source));
+            Throw.IfArgumentNegative(places, nameof(places));
+
+            // Skip the number of places rotated and yield the rest of the enumerable
+            foreach (var t in source.Skip(places))
+            {
+                yield return t;
+            }
+
+            // Go back to the beginning and return the elements skipped initially
+            var enumerator = source.GetEnumerator();
+            int i = 0;
+            do
+            {
+                if (!enumerator.MoveNext())
+                {
+                    /*
+                     * Have past the end of the collection; just break out of the loop
+                     * 
+                     * In this case, the method will have returned the original collection,
+                     * since the number of places to rotate exceeded the count
+                     */
+                    yield break;
+                }
+
+                yield return enumerator.Current;
+                i++;
+            }
+            while (i < places);
+        }
+    }
+}
